f87706efd0971858c57856f42b8b4ef3fc9dc7d1
ROCKSTAR <rayskcoc@gmail.com>
2020-05-29 21:35:50 +0530

    added app.js nad index.html

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from "react";
 import "./App.css";
+import Demo from './components/demo';
 
 class App extends React.Component {
   constructor(props) {
@@ -7,27 +8,31 @@ class App extends React.Component {
     this.state = {
       currentToDo: "",
       todos: [
-        {name: 'Consume warm water', createdAt: '01-01-2020', priorty: 'High'},
-        // "Consume warm water",
-        // "Take some fresh air",
-        // "Do some exercise",
-        // "Have your breakfast",
+        {name: 'todo1', createdAt: '01-01-2020', Priority: 'Medium'},
+        {name: 'todo2', createdAt: '01-01-2020', Priority: 'High'}
       ],
+      currentPriority : "Please select Priority"
     };
   }
 
   setValue = () => {
     let { currentToDo, todos } = this.state;
     let newTodo = todos;
-    if (currentToDo == "") {
+    if (currentToDo.trim() == "") {
       alert("It is an Empty value please enter your task");
-    } else if (newTodo.includes(currentToDo)) {
-      alert("Todo already exist");
-    } else {
-      newTodo.unshift(currentToDo);
+    }else{
+      let index = newTodo.findIndex((singleTodo)=>singleTodo.name == currentToDo);
+      if(index == -1){
+        if(this.state.currentPriority != 'Please select Priority'){
+          newTodo.unshift({name: currentToDo, createdAt: new Date().toLocaleTimeString(), Priority: this.state.currentPriority});
+          this.setState({ todos: newTodo, currentToDo: "" });
+        }else{
+          alert("Please Select Priority First");  
+        }
+      }else{
+        alert("Todo already exist");
+      }
     }
-
-    this.setState({ todos: newTodo, currentToDo: "" });
   };
 
   getValue = (event) => {
@@ -57,7 +62,12 @@ class App extends React.Component {
     let { todos } = this.state;
     todos.splice(index, 1);
     this.setState({todos});
-    // console.log('inside')
+  }
+  
+  handleOnSelect = (e) => {
+    let {currentPriority} = this.state;
+    currentPriority = e.target.value;
+    this.setState({currentPriority});
   }
 
   render() {
@@ -72,17 +82,23 @@ class App extends React.Component {
           className="input"
           placeholder="TYPE YOUR TASK"
         ></input>
+        <select className="input" value={this.state.currentPriority} onChange={this.handleOnSelect}>
+          <option disabled>Please select Priority</option>
+          {['High', 'Medium', 'Low'].map((opt, i)=><option key={i} value={opt}>{opt}</option>)}
+        </select>
         <div>
-          <button onClick={this.setValue}>Add</button>
-          <button onClick={this.removeValue}>Remove</button>
+          <button className='action-btn' onClick={this.setValue}>Add</button>
+          {/* <button className='action-btn' onClick={this.removeValue}>Remove</button> */}
+          <button className='action-btn' onClick={this.handleOnSearch}>Search</button>
         </div>
         <p>Your Todo list is here add some more : {currentToDo}</p>
-        {todos.map((v, i) => (
-          <div className='single-item'>
-            <span key={i}>{v.name} -> {v.priorty}</span>
+        {/* {todos.map((v, i) => (
+          <div className={`single-item single-item-${(v.Priority=='High'?'high':(v.Priority=='Medium')?'medium':'low')}`}>
+            <span key={i}> {v.name} -> {v.createdAt}</span>
             <button onClick={()=>this.handleRemove(i)}>x</button>
           </div>
-        ))}
+        ))} */}
+        <Demo allTodos={this.state.todos}/>
       </div>
     );
   }
